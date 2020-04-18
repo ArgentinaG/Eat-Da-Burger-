8d1976511c3e7fb906df6ecdff6f9cf30b5f0916
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-04-17 19:32:33 -0500

    established server connection

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send("hello")
+// Serve your static content utilizing use()
+// app.use(express.static("public"));
+// Parse application body
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+// Engine()
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+// Set()
+app.set("view engine","handlebars");
+// This requires your route file
+// var burgerRoute = require("./controllers/burgers_controller");
+// Utilize your routes file
+// app.use(burgerRoute);
+app.listen(PORT, () => {
+console.log("Server Works")
 });
 
-app.listen(PORT);
+
 
 
