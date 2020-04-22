cffa92672468eebd74d84f586dd1f7d9a442d75d
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-04-22 13:55:40 -0500

    managed server connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // setup the code to connect Node to MySQL.
+
+// Importing mysql package to the connection.js file
 var mysql = require("mysql");
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
   port: 3306,
-
   // Your username
   user: "root",
 
@@ -15,13 +16,17 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+
+  console.log("Connected to the Mysql server");
 });
 
 //  Export the connection  
-module.export = connection;
+module.exports = connection;
+
 
 
 
--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,63 @@
+// Object Relational Mapper (ORM)
 //* Import (require) `connection.js` into `orm.js`
+var connection = require("./connection.js");
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+
 
 // In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
 // * `selectAll()`
-// * `insertOne()`
-// * `updateOne()`
 
-// * Export the ORM object in `module.exports`.
+var orm = {
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // * `insertOne()`
+  create: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // * `updateOne()
+  update: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+}
+module.exports = orm;
